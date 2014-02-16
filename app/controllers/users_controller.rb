2c6fb271a20658403df17b5fe18f4c364ced105c
Bassem Shaker <bshakr@gmail.com>
2014-02-16 21:13:57 +0000

    Return auth_token and email when signed in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to root_url, :notice => "Signed up!"
+      render :status => 200, :json => {
+        auth_token: @user.auth_token,
+        email: @user.email
+      }
     else
       render :new
     end
