677c4572df8657305847c8eb6b85d13b33c23836
Bassem Shaker <bshakr@gmail.com>
2014-02-16 21:14:09 +0000

    validate email on create only

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   validates_presence_of :password, :on => :create
-  validates_presence_of :email
-  validates_uniqueness_of :email
+  validates_presence_of :email, :on => :create
+  validates_uniqueness_of :email, :on => :create
 
   def auth_token_expired?
     auth_token_expires_at.to_i < Time.now.to_i
@@ -11,7 +11,7 @@ class User < ActiveRecord::Base
 
   def generate_auth_token(expires = nil)
     self.auth_token = SecureRandom.hex(20)
-    self.auth_token_expires_at = expires || 1.day.from_now
+    self.auth_token_expires_at = expires || 1.year.from_now
   end
 
   def regenerate_auth_token!(expires = nil)
